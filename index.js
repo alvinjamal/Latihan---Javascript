f0dde75cd648d2576be616beb4386fcf0ffc5969
alvinjamal <alvinjamalazkya@gmail.com>
2022-09-15 14:05:37 +0700

    second commit

--- index.js
+++ index.js
@@ -44,11 +44,11 @@ cekGanjil(5);
 // Soal 3
 
 let n = 5; // height of pattern
-let segitiga = "";
+let segitiga = " ";
 // External loop
 for (let i = 0; i < 6; i++) {
   for (let j = 1; j < 5 + i - 4; j++) {
-    segitiga += j;
+    segitiga += j + " ";
   }
   segitiga += "\n";
 }
